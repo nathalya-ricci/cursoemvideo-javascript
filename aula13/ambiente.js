31bc5581b2bf406b03ce9f697ee25ef5ab137a92
Nathálya Ricci <nathalyaricci@gmail.com>
2022-06-23 17:31:04 -0300

    create aula13

---
+++ aula13/ambiente.js
@@ -0,0 +1,14 @@
+var contador = 1;
+while (contador <= 10) {
+	console.log(`Passo ${contador}`);
+	contador++; // contador = contador + 1
+}
+
+/*
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+console.log("Tudo bem?");
+*/
