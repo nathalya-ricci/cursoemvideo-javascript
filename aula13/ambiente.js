01a3f4cd7a05ba76aa372d263b9bbf31f8687181
Nathálya Ricci <nathalyaricci@gmail.com>
2022-06-27 19:46:20 -0300

    add aula14

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -1,8 +1,16 @@
 var contador = 1;
+do {
+	console.log(`Passo ${contador}`);
+	contador++;
+} while (contador <= 6);
+
+/*
+var contador = 1;
 while (contador <= 10) {
 	console.log(`Passo ${contador}`);
 	contador++; // contador = contador + 1
 }
+*/
 
 /*
 console.log("Tudo bem?");
