ee3321ecfdedcf0a90514577f19a4538aaa63592
Nathálya Ricci <nathalyaricci@gmail.com>
2022-06-30 16:07:00 -0300

    exercício concluído

--- aula14/ex016/script.js
+++ aula14/ex016/script.js
@@ -9,19 +9,22 @@ function contar() {
 		fim.value.length == 0 ||
 		passo.value.length == 0
 	) {
-		alert("[ERRO] Faltam alguns dados!");
+		res.innerHTML = "Impossível contar!";
+		// alert("[ERRO] Faltam alguns dados!");
 	} else {
-		res.innerHTML = "Contando:";
+		res.innerHTML = "Contando: <br>";
 		let i = Number(ini.value);
 		let f = Number(fim.value);
 		let p = Number(passo.value);
-
+		if (p <= 0) {
+			alert("Passo inválido! Considerando PASSO 1");
+			p = 1;
+		}
 		if (i < f) {
 			// contagem crescente
 			for (let c = i; c <= f; c += p) {
 				res.innerHTML += ` ${c} \u{1F449}`;
 			}
-			res.innerHTML += `\u{1F3C1}`;
 		} else {
 			// contagem regressiva
 			for (let c = i; c >= f; c -= p) {
